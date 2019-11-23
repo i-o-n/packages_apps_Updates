82fa221480ec38ddb486ab8a9235ab6b3acef4b5
AnkitGourav <Ankitgourav035@gmail.com>
2019-11-23 15:50:58 +0530

    Updates: Exclude only first line of changelog

--- src/com/ion/ota/UpdatesListAdapter.java
+++ src/com/ion/ota/UpdatesListAdapter.java
@@ -616,7 +616,7 @@ public class UpdatesListAdapter extends RecyclerView.Adapter<UpdatesListAdapter.
 
                 while((inputString = in.readLine()) != null) {
                     // don't include the top 4 lines of the changelog
-                    if (i >= 4) {
+                    if (i >= 1) {
                         outputString += inputString + "\n";
                     }
                     i++;
